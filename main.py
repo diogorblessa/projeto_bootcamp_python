a75155a46f6aef3058b3bb8c5416f460c7b00b8d
Diogo <diogorblessa@yahoo.com>
2026-01-22 01:52:20 -0300

    2nd commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+ # 01. Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print(len(input("Digite seu nome: ")))
+
+# 02. Crie um programa que o usuário digita dois valores e apareça a soma deles
+num1 = int(input("Digite o primeiro valor: "))
+num2 = int(input("Digite o segundo valor: "))
+print("A soma dos valores é:", num1 + num2)
+
+# 03. Refatore o exercício 01 atribuindo variáveis
+nome = input("Digite seu nome: ")
+print("O número de caracteres do seu nome é:", len(nome))
