c24db50d8ffd82205062b7ce1918d14038759881
Diogo <diogorblessa@yahoo.com>
2026-01-28 20:56:06 -0300

    Update leitor_arquivos.py

--- aula_04/leitor_arquivos.py
+++ aula_04/leitor_arquivos.py
@@ -1,26 +1,26 @@
 
-# import csv
+import csv
 
-# # Caminho para o arquivo CSV
-# caminho_arquivo = 'aula_04/exemplo.csv'
+# Caminho para o arquivo CSV
+caminho_arquivo = 'aula_04/exemplo.csv'
 
-# # Inicializa uma lista vazia para armazenar os dados
-# dados = []
+# Inicializa uma lista vazia para armazenar os dados
+dados = []
 
-# # Usa o gerenciador de contexto `with` para abrir o arquivo
-# # with abre e fecha o arquivo automaticamente; caminho_arquivo e o caminho do CSV.
-# # mode='r' abre para leitura (texto), le o conteudo e falha se o arquivo nao existir; encoding='utf-8' define a codificacao.
-# # r = read: você só vai ler o conteúdo, não pode escrever. Se o arquivo não existir, dá FileNotFoundError.
-# with open(caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
-#     # Cria um objeto leitor de CSV
-#     leitor_csv = csv.DictReader(arquivo)
+# Usa o gerenciador de contexto `with` para abrir o arquivo
+# with abre e fecha o arquivo automaticamente; caminho_arquivo e o caminho do CSV.
+# mode='r' abre para leitura (texto), le o conteudo e falha se o arquivo nao existir; encoding='utf-8' define a codificacao.
+# r = read: você só vai ler o conteúdo, não pode escrever. Se o arquivo não existir, dá FileNotFoundError.
+with open(caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
+    # Cria um objeto leitor de CSV
+    leitor_csv = csv.DictReader(arquivo)
     
-#     # Itera sobre as linhas do arquivo CSV
-#     for linha in leitor_csv:
-#         # Adiciona cada linha (um dicionário) à lista de dados
-#         dados.append(linha)
+    # Itera sobre as linhas do arquivo CSV
+    for linha in leitor_csv:
+        # Adiciona cada linha (um dicionário) à lista de dados
+        dados.append(linha)
 
-# # Exibe os dados lidos do arquivo CSV
-# for registro in dados:
-#     print(registro)
+# Exibe os dados lidos do arquivo CSV
+for registro in dados:
+    print(registro)
 
